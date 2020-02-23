76ae8e2b25c32a841d79060fad7c0c810c52bcb4
Takumi Yamada <xirtardauq@gmail.com>
2020-02-23 13:39:46 +0900

    SearchBox => NewsSearch

--- src/components/NewsSearch/index.js
+++ src/components/NewsSearch/index.js
@@ -9,7 +9,7 @@ import { loader } from 'graphql.macro';
 // const Query = gql`query {...}` のように定義も可能です。
 const SearchNewsQuery = loader('./query.graphql');
 
-const SearchBox = (props) => {
+const NewsSearch = (props) => {
   // コンポーネント内で状態を持ちたい場合はuseStateを使用します。
   // 状態保持用の変数と、状態更新用の関数を取得できます。
   const [searchTerm, setSearchTerm] = useState("");
@@ -42,4 +42,4 @@ const SearchBox = (props) => {
   )
 };
 
-export default SearchBox;
+export default NewsSearch;
